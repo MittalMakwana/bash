69a4dff3c92a833111e074f1236b10f1e409328d
Mittal Makwana <mmakwana@mmakwana.local>
2018-04-09 20:34:13 -0400

    Added documentation to ping list

--- ping-list.bash
+++ ping-list.bash
@@ -1,13 +1,37 @@
 #!/bin/bash
+## 
+## Author: Mittal Makwana
+## Email: mittalmak@gmail.com
+##
+##
+
+## The following for loops take a filename as an arguement 
+## The file name should contain IP address/domain name
+## The for loop itrates over the list and ping each server
+## one at a time and save the result
+## if sucessfull to ping_good file
+## and fail ping server are storage in ping_bad file
+##
+##
+##
+
+#########################
+# Declare parameter
+########################
+
+good="ping_good"
+bad="ping_bad"
+ping_count=1
+
 
 for _IP in $(cat ${1})
 do
-ping -q -c 1 $_IP > /dev/null
+ping -q -c $ping_count $_IP > /dev/null
 if [ $? -eq 0 ]
 then
-   echo "$_IP" >>ping_good
+   echo "$_IP" >> $good
 else
-   echo "$_IP" >>ping_bad
+   echo "$_IP" >> $bad
 fi
 done
 
